93016b8b9ed2ca8957b58cd6d4ceefedb01a973b
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-02-13 21:01:31 +0100

    Add training pipeline on the CPU

--- main.jl
+++ main.jl
@@ -1,6 +1,7 @@
 using MLDatasets
 using Flux, Statistics
 using Flux.Data: DataLoader
+using Flux.Optimise: Optimiser
 using Flux: onehotbatch, onecold, @epochs
 using Flux.Losses: logitcrossentropy
 using Base: @kwdef
@@ -9,6 +10,8 @@ using MLDatasets
 
 
 function get_data(args)
+    # it fixes the error "DataType has no batchsize"
+    ENV["DATADEPS_ALWAYS_ACCEPT"] = "true"
 
     # load train and test dataset
     x_train, y_train = MNIST.traindata()
@@ -16,6 +19,9 @@ function get_data(args)
 
     # Reshape arbitrarly-shaped input into a matrix-shaped output
     x_train, x_test = Flux.flatten(x_train), Flux.flatten(x_test)
+
+    # One-hot-encode the labels
+    y_train, y_test = onehotbatch(y_train, 0:9), onehotbatch(y_test, 0:9)
 
     train_loader = DataLoader((x_train, y_train), batchsize=args.batchsize, shuffle=true)
     test_loader = DataLoader((x_test, y_test), batchsize=args.batchsize)
@@ -30,7 +36,7 @@ function loss_function(ŷ, y)
 end
 
 
-function evaluation_loss_accuracy(loader, model, device)
+function evaluation_loss_accuracy(loader, model)
 
     loss, accuracy, counter = 0f0, 0f0, 0
 
@@ -44,3 +50,50 @@ function evaluation_loss_accuracy(loader, model, device)
     return loss / counter, accuracy / counter
 end
 
+
+function set_model()
+end
+
+
+# arguments for the `train` function 
+Base.@kwdef mutable struct Args
+    η = 1e-4             # learning rate
+    batchsize = 128      # batch size
+    epochs = 10          # number of epochs
+end
+
+function train(; kws...)
+    args = Args(; kws...) # collect options in a struct for convenience
+    
+    train_loader, test_loader = get_data(args)
+
+    model = set_model()
+
+    ps = Flux.params(model)  
+    opt = ADAM(args.η) 
+
+    for epoch in 1:args.epochs
+
+        for (x,y) in train_loader
+            gs = Flux.gradient(ps) do
+                ŷ = model(x)
+                loss_function(ŷ, y)
+            end
+
+            Flux.Optimise.update!(opt, ps, gs)
+        end
+
+        train = evaluation_loss_accuracy(train_loader, model)
+        test = evaluation_loss_accuracy(test_loader, model)
+
+        println("Epoch $(epoch-1)")
+        println("\t Train => loss = $(train[1]) \t acc = $(train[2])")
+        println("\t Test => loss = $(test[1]) \t acc = $(test[2])")
+    
+    end
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    train()
+end
