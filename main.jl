d80aaa7a80c54e9977336ab58d72bd61e7307551
francescodisalvo05 <francesco.disalvo99@gmail.com>
2022-02-17 09:06:40 +0100

    Clean code

--- main.jl
+++ main.jl
@@ -58,7 +58,7 @@ function evaluation_loss_accuracy(loader, model)
     return loss / counter, accuracy / counter
 end
 
-# my implementation
+
 function set_model(; size=(256, 256,3), num_classes=10)
     return Chain(
                 Conv((11, 11), 3=>64, stride=(4,4), relu, pad=(2,2)),
@@ -78,49 +78,13 @@ function set_model(; size=(256, 256,3), num_classes=10)
                 Dense(4096, num_classes))
 end
 
-# https://github.com/FluxML/Metalhead.jl/blob/master/src/convnets/alexnet.jl#L10-L28
-function alexnet(; nclasses = 10)
-    layers = Chain(Chain(Conv((11, 11), 3 => 64, stride = (4, 4), relu, pad = (2, 2)),
-                         MaxPool((3, 3), stride = (2, 2)),
-                         Conv((5, 5), 64 => 192, relu, pad = (2, 2)),
-                         MaxPool((3, 3), stride = (2, 2)),
-                         Conv((3, 3), 192 => 384, relu, pad = (1, 1)),
-                         Conv((3, 3), 384 => 256, relu, pad = (1, 1)),
-                         Conv((3, 3), 256 => 256, relu, pad = (1, 1)),
-                         MaxPool((3, 3), stride = (2, 2)),
-                         AdaptiveMeanPool((6,6))),
-                   Chain(flatten,
-                         Dropout(0.5),
-                         Dense(256 * 6 * 6, 4096, relu),
-                         Dropout(0.5),
-                         Dense(4096, 4096, relu),
-                         Dense(4096, nclasses)))
-  
-    return layers
-end
-
-# https://github.com/FluxML/model-zoo/blob/master/vision/conv_mnist/conv_mnist.jl
-function LeNet5(; imgsize=(64,64,3), nclasses=10) 
-    out_conv_size = (imgsize[1]÷4 - 3, imgsize[2]÷4 - 3, 16)
-    
-    return Chain(
-            Conv((5, 5), imgsize[end]=>6, relu),
-            MaxPool((2, 2)),
-            Conv((5, 5), 6=>16, relu),
-            MaxPool((2, 2)),
-            flatten,
-            Dense(prod(out_conv_size), 120, relu), 
-            Dense(120, 84, relu), 
-            Dense(84, nclasses)
-          )
-end
-
 
 Base.@kwdef mutable struct Args
     η = 1e-4             # learning rate
     batchsize = 128     # batch size
     epochs = 1          # number of epochs
 end
+
 
 function train(; kws...)
     args = Args(; kws...) # collect options in a struct for convenience
